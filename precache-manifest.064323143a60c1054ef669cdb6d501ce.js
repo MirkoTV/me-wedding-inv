f8199cc1cfb3cca93922f5a2b0c2876af76a061d
Mirko Terrazas <mirko.terrazas@buildout.com>
2022-07-01 23:43:10 -0400

    Updates

---
+++ precache-manifest.064323143a60c1054ef669cdb6d501ce.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b4241d6faa9b015fc28a536697272f",
+    "url": "/me-wedding-inv/index.html"
+  },
+  {
+    "revision": "6d239a6f15dccda37cf0",
+    "url": "/me-wedding-inv/static/css/2.4ed7ca72.chunk.css"
+  },
+  {
+    "revision": "1f21f89c7a62ce4e093a",
+    "url": "/me-wedding-inv/static/css/main.f0afc3af.chunk.css"
+  },
+  {
+    "revision": "6d239a6f15dccda37cf0",
+    "url": "/me-wedding-inv/static/js/2.83253973.chunk.js"
+  },
+  {
+    "revision": "1f21f89c7a62ce4e093a",
+    "url": "/me-wedding-inv/static/js/main.cbe0cd4f.chunk.js"
+  },
+  {
+    "revision": "e0ac8198cdddd64748ef",
+    "url": "/me-wedding-inv/static/js/runtime~main.bc653868.js"
+  },
+  {
+    "revision": "4fecfaadb34f86867d0c7ae5c25e315f",
+    "url": "/me-wedding-inv/static/media/Background_Circle.4fecfaad.png"
+  },
+  {
+    "revision": "9b14f9e6765b6de220768a0df0fc9109",
+    "url": "/me-wedding-inv/static/media/a123.9b14f9e6.png"
+  },
+  {
+    "revision": "8e648955fe15933644da07a2df19cdba",
+    "url": "/me-wedding-inv/static/media/ap.8e648955.png"
+  },
+  {
+    "revision": "155af7fd1e56340dc2cec7bd2761e203",
+    "url": "/me-wedding-inv/static/media/brideback.155af7fd.jpg"
+  },
+  {
+    "revision": "0848d9d0f415ea63d3ecce98e586b461",
+    "url": "/me-wedding-inv/static/media/counter-1-1600x600.0848d9d0.jpg"
+  },
+  {
+    "revision": "82c8fbe284a653442e684c21bbe4336c",
+    "url": "/me-wedding-inv/static/media/logo-white.82c8fbe2.png"
+  },
+  {
+    "revision": "63c9121422bb2aeb0a785bac373f9f2b",
+    "url": "/me-wedding-inv/static/media/mana2.63c91214.jpg"
+  },
+  {
+    "revision": "ff5e1123b4ec5c35c81ed20ed7fb81f6",
+    "url": "/me-wedding-inv/static/media/manoj1.ff5e1123.png"
+  },
+  {
+    "revision": "28e168db532d1ad295f4ed26b6eb4f51",
+    "url": "/me-wedding-inv/static/media/met3.28e168db.jpg"
+  },
+  {
+    "revision": "0c07687b15a0a7867c665a2826dd9b86",
+    "url": "/me-wedding-inv/static/media/met4.0c07687b.jpg"
+  },
+  {
+    "revision": "20143035b776dcefdba8e40456a55df1",
+    "url": "/me-wedding-inv/static/media/met5.20143035.jpg"
+  },
+  {
+    "revision": "bb5c4c8f4b999520f41db44349e2d28f",
+    "url": "/me-wedding-inv/static/media/slider-1-1600x900.bb5c4c8f.jpg"
+  },
+  {
+    "revision": "32d27b8e2d1309228e888d54285197d3",
+    "url": "/me-wedding-inv/static/media/w.32d27b8e.png"
+  },
+  {
+    "revision": "f6790001d9d79873821dff15decac8d0",
+    "url": "/me-wedding-inv/static/media/w22.f6790001.png"
+  },
+  {
+    "revision": "1f0312b65ec66e7a3ade40aecf48a6af",
+    "url": "/me-wedding-inv/static/media/webzed-font-1.1f0312b6.woff"
+  },
+  {
+    "revision": "6ed2107f2da89dcf9e52f006e878b679",
+    "url": "/me-wedding-inv/static/media/webzed-font-1.6ed2107f.ttf"
+  },
+  {
+    "revision": "d5939ecfb23c9b4504264e0c4234ba30",
+    "url": "/me-wedding-inv/static/media/webzed-font-1.d5939ecf.eot"
+  },
+  {
+    "revision": "e65681d7a99427774c50261520216889",
+    "url": "/me-wedding-inv/static/media/webzed-font-1.e65681d7.svg"
+  }
+]);
