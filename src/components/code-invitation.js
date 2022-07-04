d0e99368cdf3ec43e837fd00b4272cfc0d4b308f
Mirko Terrazas <mirko.terrazas@buildout.com>
2022-07-04 13:39:55 -0400

    Fix invitation code

--- src/components/code-invitation.js
+++ src/components/code-invitation.js
@@ -14,6 +14,9 @@ require 'friends'
 erika = Bride.new
 mirko = Groom.new
 
+TEMPLO_CALA_CALA_LOCATION = 'https://goo.gl/maps/Yh2VeDCihbYLkWdE8'
+SALON_LA_TERRAZA_LOCATION = 'https://goo.gl/maps/RdySP3MADXkjn9h28'
+
 class Wedding < ActiveEngagement::Base
 
   def invite
@@ -24,38 +27,69 @@ class Wedding < ActiveEngagement::Base
                   and
             Erika Rosaed Vargas Navarro
         On the 24th of September 2022
-        At the xxxx xxxx, Leeds
 ----------------------------------------------------------
-                               _      Mirko y Erika
-         ___            {@}  _|=|_
-        /___\\          /(")\\  (")
-     .---'-'---.      /((~))\\/<x>\\        _   .-.
-    /___________\\     ~~/@\\~~\\|_|/       <v> ((_))
-     | A /^\\ A |       /   \\  |||       ((_)) '-'
-     |   |"|   |      /~@~@~\\ |||        '-'
- ____|___|_|___|_____/_______\\|||___October 25th 2013___
+                                _      Mirko y Erika
+         ___             {@}  _|=|_
+        /___\\           /(")\\  (")
+      .---'-'---.      /((~))\\/<x>\\        _   .-.
+    /___________\\      ~~/@\\~~\\|_|/       <v> ((_))
+      | A /^\\ A |       /   \\  |||       ((_)) '-'
+      |   |"|   |      /~@~@~\\ |||        '-'
+  ____|___|_|___|_____/_______\\|||___September 24th 2022___
         Ceremony Commences at 13:00 pm
           Followed by a
         Wedding & Evening Reception
     INVITE
-
-    if self.internet_access?
-        redirect_to 'http://heatheranddom.com/rsvp'
-      else
-        rsvp_to 'Mr + Mrs Burke, Address Here'
-    end
   end
 
   def ceremony
     begin
       Ceremony.start
-      erika.kissed_by(mirko)
-      Ceremony.sign_register(erika, mirko)
-      erika.surname! = "Vargas"
-      throw :bouquet
+
+      religious_wedding(
+        DateTime.new(2022, 09, 24, 13, 00, 00), TEMPLO_CALA_CALA_LOCATION)
+      reception(
+        DateTime.new(2022, 09, 24, 15, 00, 00), SALON_LA_TERRAZA_LOCATION)
+      civil_wedding(
+        DateTime.new(2022, 09, 24, 16, 30, 00))
+      dinner(
+        DateTime.new(2022, 09, 24, 19, 00, 00))
+      party!
+
       Ceremony.end
     rescue AnyLawfulImpediment
       # This should never happen!
+    end
+  end
+
+  private
+
+  def religious_wedding(date, location)
+    Logger.log('Starting religious wedding at ', date)
+    Ceremony.move_to(location)
+
+    return unless erika.say_yes?
+
+    erika.kissed_by(mirko)
+  end
+
+  def reception(date, locaiton)
+    Logger.log('Receiving friends and family ', date)
+    Ceremony.move_to(location)
+  end
+
+  def civil_wedding(date)
+    Logger.log('Starting civil wedding at ', date)
+
+    Ceremony.sign_register(erika, mirko)
+    erika.surname += "de Terrazas" if erika.happy?
+  end
+
+  def party!
+    begin
+      throw :bouquet
+    rescue
+      Ceremony.next!
     end
   end
 end
